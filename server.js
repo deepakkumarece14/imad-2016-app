7288a2249b598caefad22827047848074cfde0fb
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-06 16:54:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -25,8 +29,8 @@ app.get('/ui/madi.png', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/background.jpeg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'background.jpeg'));
+app.get('/ui/background.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'background.jpg'));
 });
 
 app.get('/ui/profile.jpg', function (req, res) {
