fa3a3817d1ff0ebc9cac002d97143876869f2b3f
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-07 20:28:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articlehtml = {
+    title: 'Article HTML',
+    date: '11th Nov. 2016',
+    content: ` 
+		<p>CSS stands for Cascading Style Sheet.</p>
+		<div class="info"><p><b>info</b> goes here</p></div>`
+};
+
+var htmlTemplate = `<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="/ui/madi.png" />
+        <link rel="stylesheet" href="/ui/style.css" />
+    </head>
+<body>
+<div class="menubar_container">
+	<ul class="menubar_left" style="width:100%;overflow:hidden;height:50px">
+		<li class="menu_left"><a href="#menu"><img src="/ui/menu-3.png"/></a></li>
+		<li class="menu_left"><a href="/ui/home_page.html">HOME</a></li>
+		<li class="menu_left"><a href="/ui/profile.html">PROFILE</a></li>
+		<li class="menu_left"><a href="/ui/articles.html">ARTICLES</a></li>
+		
+		<li><input class="search_box menubar_right menu_right" type="text" name="search_box" placeholder="Search" height="45px"></li>
+		<li class="menubar_right menu_right"><img src="/ui/searchicon.png"/></li>
+		<li class="menubar_right menu_right"><a href="/ui/user_login.html" target="_blank" title="Login">
+		    <img src="/ui/users.png"/></a></li>
+		<li class="menu_right"><a href="/ui/feedback.html" target="_blank" title="Feedback">FEEDBACK</a></li>
+	</ul>
+</div> <!--end of menubar_container-->
+
+<div class="body_container">
+	<p></p>
+	<h2>${title}</h2>
+	<div class="date">${date}</div>
+	<div class="content">
+		${content}
+	</div>
+</div>
+
+<script lang="javascript" src="/ui/main.js"></script>
+</body>
+</html>`;
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
