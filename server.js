9171822f5000b865c0d25a88381f0a0105ed1aa7
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-08 10:11:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    articlehtml: {
+    'article-html': {
         title: 'Article HTML',
         heading: 'Article HTML',
         date: '24th Sep. 2016',
