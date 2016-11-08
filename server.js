58708aa90fda65ce439d7a623377eef9d4aa309b
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-09 00:52:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/:articleName', function (req, res) {
 var count = 0;
 app.get('/counter', function (req, res) {
     count = count + 1;
-    res.send(count.toString())
+    res.send(count.toString());
 });
 
 app.get('/ui/madi.png', function (req, res) {
