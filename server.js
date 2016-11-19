2ef7a479b76b3236e4a67f62d00ec50680c19c61
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-19 22:30:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articleName));
 });
 
 var count = 0;
