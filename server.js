f465ec138841846c61c38d34fbb4a453d2f4a30f
deepakkumarece14 <deepakkumar.ece14@nituk.ac.in>
2016-11-19 23:29:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/articles/:articleName', function (req, res) {
   });
 });*/
 
-app.get('/articles/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
