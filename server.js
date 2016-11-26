c835b06a864bd44fcc081428400488357dd71647
Deepak Kumar <deepakkumar.ece14@nituk.ac.in>
2016-11-26 23:32:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ app.post('/create-user', function (req, res) {
 });
 
 
-app.post('/userlogin', function (req, res) {
+app.post('/login', function (req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
 		
